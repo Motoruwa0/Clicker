05f17b9559a614b0560cd47b214ec706b7a6e985
Motoruwa0 <michal3.wegrzyn@wp.pl>
2026-01-09 14:16:51 +0100

    Delete Clicker/js directory

--- Clicker/js/state.js
+++
@@ -1,6 +0,0 @@
-const gameState = {
-  cookies: 0,
-  cookiesPerSecond: 0,
-  totalClicks: 0,
-  totalCookies: 0
-};
--- Clicker/js/upgrades.js
+++
@@ -1,5 +0,0 @@
-const upgrades = [
-  { name: 'Babuszka', cost: 15, cps: 1, count: 0, emoji: '👵' },
-  { name: 'Lokaj Batmana', cost: 100, cps: 8, count: 0, emoji: '🦇' },
-  { name: 'Pomidor', cost: 50000, cps: 2137, count: 0, emoji: '🍅' }
-];
