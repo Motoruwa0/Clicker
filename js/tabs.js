5dafeddbdbd8c857e69d32ac53975ed332a56138
Motoruwa0 <michal3.wegrzyn@wp.pl>
2026-01-12 14:08:07 +0100

    Update tabs.js

--- js/tabs.js
+++ js/tabs.js
@@ -5,20 +5,21 @@ buttons.forEach(button => {
   button.addEventListener("click", () => {
     const tabId = button.dataset.tab;
 
-    // aktywna zakładka
+
     tabs.forEach(tab => {
       tab.classList.toggle("tab--active", tab.id === tabId);
     });
 
-    // aktywny przycisk
+    
     buttons.forEach(btn => {
       btn.classList.toggle("nav__btn--active", btn === button);
     });
 
-    // HUD tylko w Ulepszeniach
+   
     document.body.classList.toggle(
       "show-upgrades-hud",
       tabId === "upgrades"
     );
   });
 });
+
