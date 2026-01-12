8e7d3c0e831517c931adbc6931f1c7919c629bcc
Gitarowy-pomidor <marcinteclaw8@gmail.com>
2026-01-12 09:10:55 +0100

    Upgrades12

--- js/upgrades.js
+++ js/upgrades.js
@@ -1,12 +1,11 @@
 const upgrades = [
   { name: "Kursor", cps: 1, baseCost: 10, count: 0 },
-  { name: "Robot", cps: 10, baseCost: 100, count: 0 },
-  { name: "Fabryka", cps: 20, baseCost: 300, count: 0 },
-  { name: "Kopalnia", cps: 50, baseCost: 800, count: 0 },
-  { name: "Elektrownia", cps: 100, baseCost: 2000, count: 0 },
-  { name: "Miasto", cps: 200, baseCost: 5000, count: 0 },
-  { name: "Państwo", cps: 300, baseCost: 10000, count: 0 },
-  { name: "Kontynent", cps: 400, baseCost: 20000, count: 0 },
-  { name: "Planeta", cps: 500, baseCost: 50000, count: 0 },
-  { name: "Wszechświat", cps: 1000, baseCost: 150000, count: 0 }
+  { name: "Babuszka", cps: 10, baseCost: 100, count: 0 },
+  { name: "Trenbolon", cps: 50, baseCost: 800, count: 0 },
+  { name: "El Gato", cps: 100, baseCost: 2000, count: 0 },
+  { name: "Trevor", cps: 252, baseCost: 5000, count: 0 },
+  { name: "Gofry z bitą śmietaną", cps: 400, baseCost: 20000, count: 0 },
+  { name: "Pomidor", cps: 500, baseCost: 50000, count: 0 },
+  { name: "KASYNO", cps: 2137, baseCost: 150000, count: 0 },
+  {name: "Tereska67", cps: 6767, baseCost: 200000, count: 0 }
 ];
