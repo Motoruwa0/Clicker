a5344fff1b1dcf9a485314f94ba3de1211a6f196
LSDINf <vitalikosaulenko2008@gmail.com>
2026-01-16 11:07:55 +0100

    ulepszenie exp

--- js/exp.js
+++ js/exp.js
@@ -1,6 +1,6 @@
 function addXPFromClicks() {
  
-  const gainedXP = Math.floor(state.clicks / 1) - state._xpFromClicks;
+  const gainedXP = Math.floor(state.clicks / 1.5) - state._xpFromClicks;
 
   if (gainedXP > 0) {
     state._xpFromClicks += gainedXP;
