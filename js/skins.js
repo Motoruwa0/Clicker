035cadc51bdc8c79c19acb97161e4decba1918e0
Motoruwa0 <michal3.wegrzyn@wp.pl>
2026-01-28 12:15:35 +0100

    zmiana cen ulepszen oraz poprawy w skinach

--- js/skins.js
+++ js/skins.js
@@ -36,196 +36,203 @@ const skins = [
     name: "Pomidorek",
     price: 750,
     img: "images/skin19.png",
-    requiredLevel: 15
+    requiredLevel: 10
   },
   {
     id: "skin-9",
     name: "Będzie git",
     price: 1000,
     img: "images/Skin9.png",
-    requiredLevel: 15
+    requiredLevel: 10
   },
   {
     id: "skin-6",
     name: "Bomboclat cat",
     price: 1500,
     img: "images/Skin4.png",
-    requiredLevel: 15
+    requiredLevel: 10
   },
   {
     id: "skin-7",
     name: "Mysi Ksiądz",
     price: 2000,
     img: "images/Skin5.png",
-    requiredLevel: 15
+    requiredLevel: 10
   },
   {
     id: "skin-8",
     name: "EL Gato",
     price: 2500,
     img: "images/Skin8.png",
-    requiredLevel: 20
+    requiredLevel: 10
   },
   {
     id: "skin-10",
     name: "Koto-pomarańcza",
     price: 5000,
     img: "images/Skin10.png",
-    requiredLevel: 20
+    requiredLevel: 10
   },
   {
     id: "skin-11",
     name: "Koto-avocado",
     price: 5001,
     img: "images/Skin11.png",
-    requiredLevel: 20
+    requiredLevel: 10
   },
   {
     id: "skin-29",
     name: "Frytek",
     price: 5023,
     img: "images/skin29.png",
-    requiredLevel: 20
+    requiredLevel: 10
   },
   {
     id: "skin-12",
     name: "Kot gamer",
     price: 7500,
     img: "images/Skin12.png",
-    requiredLevel: 25
+    requiredLevel: 10
   },
   {
     id: "skin-21",
     name: "Patryk mózgowiec",
     price: 10000,
     img: "images/skin21.png",
-    requiredLevel: 25
+    requiredLevel: 10
   },
   {
     id: "skin-23",
     name: "Smutny chomik",
     price: 12500,
     img: "images/skin23.png",
-    requiredLevel: 25
+    requiredLevel: 10
   },
   {
     id: "skin-22",
     name: "MaŁy kucharz",
     price: 15000,
     img: "images/skin22.png",
-    requiredLevel: 25
+    requiredLevel: 10
   },
   {
     id: "skin-24",
     name: "Ogórkowy kot",
     price: 20000,
     img: "images/skin24.png",
-    requiredLevel: 30
+    requiredLevel: 10
   },
   {
     id: "skin-20",
     name: "Eric Cartman",
     price: 30000,
     img: "images/skin20.png",
-    requiredLevel: 30
+    requiredLevel: 10
   },
   {
     id: "skin-28",
     name: "Szafuńcio",
     price: 35550,
     img: "images/skin28.png",
-    requiredLevel: 30
+    requiredLevel: 10
   },
   {
     id: "skin-13",
     name: "Freaky Kitku",
     price: 40000,
     img: "images/Skin13.png",
-    requiredLevel: 30
+    requiredLevel: 20
   },
   {
     id: "skin-14",
     name: "Nerd",
     price: 40001,
     img: "images/Skin14.png",
-    requiredLevel: 40
+    requiredLevel: 20
   },
   {
     id: "skin-15",
     name: "Koto-lizak",
     price: 50000,
     img: "images/Skin15.png",
-    requiredLevel: 40
+    requiredLevel: 20
   },
   {
     id: "skin-27",
     name: "Kot w kulkach",
     price: 75000,
     img: "images/skin27.png",
-    requiredLevel: 40
+    requiredLevel: 20
   },
   {
     id: "skin-31",
     name: "Pesek",
     price: 95000,
     img: "images/skin31.png",
-    requiredLevel: 40
+    requiredLevel: 20
   },
   {
     id:"skin30",
     name: "Kula mocy",
     price: 100002,
     img: "images/skin30.png",
-    requiredLevel: 50
+    requiredLevel: 30,
+    requiredRebirths: 1
   },
   {
     id: "skin-25",
     name: "Kwietny pusheen",
     price: 100000,
     img: "images/skin25.png",
-    requiredLevel: 50
+    requiredLevel: 30,
+    requiredRebirths: 1
   },
   {
     id: "skin-4",
     name: "Kot Na Tronie",
     price: 1000000,
     img: "images/Skin7.png",
-    requiredLevel: 50
+    requiredLevel: 30,
+    requiredRebirths: 1
   }, 
   {
     id: "skin-26",
     name: "Kremówka",
     price: 22113377,
     img: "images/skin26.png",
-    requiredLevel: 50
+    requiredLevel: 30,
+    requiredRebirths: 1
   },
   {
     id: "skin-32",
     name: "Jägermeister kitku",
     price: 1000000,
     img: "images/skin32.png",
-    requiredLevel: 60
+    requiredLevel: 40,
+    requiredRebirths: 2
   }, 
   {
     id: "skin-33",
     name: "HAPPY MEAL",
     price: 1000000,
     img: "images/skin33.png",
-    requiredLevel: 60
+    requiredLevel: 40,
+    requiredRebirths: 2
   }, 
   {
     id: "skin-34",
     name: "Monster kitku",
     price: 1000000,
     img: "images/skin34.png",
-    requiredLevel: 60
+    requiredLevel: 40,
+    requiredRebirths: 2
   }, 
   {
     id: "skin-35",
     name: "Romper kitku",
     price: 1000000,
     img: "images/skin35.png",
-    requiredLevel: 61,
+    requiredLevel: 40,
     requiredRebirths: 2
   }
 ];
--- js/upgrades.js
+++ js/upgrades.js
@@ -12,7 +12,7 @@ const upgrades = [
   name: "Lepszy kursor 2.0",
   type: "click",
   value: 10,
-  baseCost: 150,
+  baseCost: 1500,
   count: 0,
   cps: 0,
   requiredLevel: 5
@@ -21,16 +21,16 @@ const upgrades = [
 
 
   { name: "Żepyga", cps: 1, baseCost: 50, count: 0, requiredLevel: 5 },
-  { name: "Babuszka", cps: 10, baseCost: 200, count: 0, requiredLevel: 10 },
-  { name: "Trenbolon", cps: 350, baseCost: 800, count: 0, requiredLevel: 15 },
-  { name: "El Gato", cps: 120, baseCost: 2500, count: 0, requiredLevel: 20 },
-  { name: "Trevor", cps: 300, baseCost: 6000, count: 0, requiredLevel: 25 },
-  { name: "Gofry z bitą śmietaną", cps: 600, baseCost: 15000, count: 0, requiredLevel: 30 },
-  { name: "Pomidor", cps: 1000, baseCost: 40000, count: 0, requiredLevel: 35 },
-  { name: "KASYNO", cps: 2500, baseCost: 120000, count: 0, requiredLevel: 40 },
-  { name: "Tereska67", cps: 6000, baseCost: 300000, count: 0, requiredLevel: 45 },
-  { name: "Fryteczki Michała", cps: 9000, baseCost: 700000, count: 0, requiredLevel: 50},
-  { name: "Miszka lubi jabluko", cps: 15000, baseCost: 1500000, count: 0, requiredLevel: 55},
-  { name: "Żaba z Krakowa", cps: 30000, baseCost: 3000000, count: 0, requiredLevel: 60}
+  { name: "Babuszka", cps: 10, baseCost: 500, count: 0, requiredLevel: 10 },
+  { name: "El Gato", cps: 120, baseCost: 4000, count: 0, requiredLevel: 15 },
+  { name: "Trenbolon", cps: 350, baseCost: 5000, count: 0, requiredLevel: 20 },
+  { name: "Trevor", cps: 300, baseCost: 15000, count: 0, requiredLevel: 20 },
+  { name: "Gofry z bitą śmietaną", cps: 600, baseCost: 80000, count: 0, requiredLevel: 30 },
+  { name: "Pomidor", cps: 1000, baseCost: 400000, count: 0, requiredLevel: 30 },
+  { name: "KASYNO", cps: 2500, baseCost: 1000000, count: 0, requiredLevel: 40 },
+  { name: "Tereska67", cps: 6000, baseCost: 2000000, count: 0, requiredLevel: 40 },
+  { name: "Fryteczki Michała", cps: 9000, baseCost: 7000000, count: 0, requiredLevel: 40, requiredRebirths: 1},
+  { name: "Miszka lubi jabluko", cps: 15000, baseCost: 15000000, count: 0, requiredLevel: 40, requiredRebirths: 1},
+  { name: "Żaba z Krakowa", cps: 30000, baseCost: 30000000, count: 0, requiredLevel: 40, requiredRebirths: 2}
 ];
 
